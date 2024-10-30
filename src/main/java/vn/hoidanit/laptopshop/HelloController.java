97c78c3a7f5da4655a0768ecfe3675692261a0bd
Nguyen Dinh Luong <dinhluong19002004@gmail.com>
2024-10-30 23:10:06 +0700

    video 17

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,16 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloController {
+    
+    @GetMapping
+    public String index() {
+        return "Hello world I am from VietNam";
+    }
+
+}
