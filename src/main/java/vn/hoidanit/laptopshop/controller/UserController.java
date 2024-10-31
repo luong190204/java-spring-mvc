6e1f95582c6d1cb94a782157d41e8da8a3dfb3e1
Nguyen Dinh Luong <dinhluong19002004@gmail.com>
2024-10-31 22:15:35 +0700

    setup jsp

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,21 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
-    public String getHomePage() {
-        return "index.html";
+    public String getHomePage(Model model) {
+
+        String test = userService.handleHello();
+
+        model.addAttribute("dinhluong", test);
+        model.addAttribute("luong", "neu em la mot ang may trang");
+
+        return "index";
     }
 }
 
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -4,11 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Document</title>
 </head>
 
 <body>
-    hello from view html
+    Date of birth
+
+    <h1>
+        ${dinhluong}
+    </h1>
+    <h1>
+        ${luong}
+    </h1>
 </body>
 
 </html>
